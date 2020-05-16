d8904c138b52acd4d696c276f232e19f2675278e
Daniel Ronan <you@example.com>
2020-05-16 15:34:31 +0100

    finished css

--- js/index.js
+++ js/index.js
@@ -1,28 +1,4 @@
-// !! IMPORTANT README:
+var e=document.getElementById("email"),t=document.getElementById("form");o.assert.strictEqual(t.hasAttribute("action"),!0,"The #form should have an action attribute "),o.assert.include(t.action,"http","The action attribute's value should be a url (http...) "),o.assert.strictEqual(e.hasAttribute("name"),!0,"The #email input should have a name attribute "),o.assert.strictEqual(e.name,"email",'The #email element\'s name attribute should have a value of "email" ')
+var e=document.getElementById("email");o.assert.strictEqual(e.type,"email","The #email input element should use HTML5 validation ")
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
-
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
 "use strict";
